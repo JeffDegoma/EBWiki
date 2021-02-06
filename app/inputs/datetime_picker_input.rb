99e8f2bc2dacc9f5f7ee8b5ee834dd758cc66bca
Mark Nyon <mark@grandkru.com>
2021-02-06 10:52:18 -0500

    Removed rubocop individual errors #3852

--- app/inputs/datetime_picker_input.rb
+++ app/inputs/datetime_picker_input.rb
@@ -5,12 +5,14 @@ class DatetimePickerInput < DatePickerInput
   private
 
   def display_pattern
-    I18n.t('datepicker.dformat', default: '%d/%m/%Y') + ' ' +
-      I18n.t('timepicker.dformat', default: '%R')
+    date = "#{I18n.t('datepicker.dformat', default: '%d/%m/%Y')}"
+    time = "#{I18n.t('timepicker.dformat', default: '%R')}"
+    "#{date} #{time}"
   end
 
   def picker_pattern
-    I18n.t('datepicker.pformat', default: 'DD/MM/YYYY') + ' ' +
-      I18n.t('timepicker.pformat', default: 'HH:mm')
+    date = "#{I18n.t('datepicker.pformat', default: 'DD/MM/YYYY')}"
+    time = "#{I18n.t('timepicker.pformat', default: 'HH:mm')}"
+    "#{date} #{time}"
   end
 end
